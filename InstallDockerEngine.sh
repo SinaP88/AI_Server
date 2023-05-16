3aea0b1dd67fca95b781b6bb4b2ce0ec4fe5f5ee
Sina Parchami <sina.parchami@orcadent.de>
2023-05-16 12:25:34 +0200

    bugfix server

--- InstallDockerEngine.sh
+++ InstallDockerEngine.sh
@@ -10,7 +10,7 @@ sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libx
 ## Set up the repository
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 # Add Docker’s official GPG key:
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -24,7 +24,7 @@ echo \
 #Update the apt package index:
 sudo apt-get update
 # Install Docker Engine, containerd, and Docker Compose.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 echo "==========================="
 echo "  Docker Engine Installed  "
 echo "==========================="
--- app/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export miniconda_dir=/root/miniconda
-systemctl stop opgflask
-cd /root
-. $miniconda_dir/bin/deactivate OPG
-yes | conda remove --name OPG --all
-
-
